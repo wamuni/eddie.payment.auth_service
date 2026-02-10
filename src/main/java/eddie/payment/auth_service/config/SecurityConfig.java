c5b4e6b05670562196d0a63e157b3f2ab045e6c7
wamuni <boshi.pan@outlook.com>
2026-02-10 23:49:02 +0000

    first controller and security config

---
+++ src/main/java/eddie/payment/auth_service/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package eddie.payment.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/health", "/.well-known/jwks.json").permitAll()
+				.anyRequest().authenticated()
+			);
+		return http.build();
+	}
+}
